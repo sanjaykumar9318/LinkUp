0ccc04c7f38d55358687ddd7ab948d6d93598d18
sanjaykumar9318 <sanjayswiz26@gmail.com>
2026-07-16 00:45:32 +0530

    prodcution setup error

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,9 +7,10 @@ import cookieParser from "cookie-parser"
 import cors from "cors";
 import { app,server } from "./lib/socket.js"
 import path from "path"
-
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 dotenv.config();
-const __dirname = path.resolve;
 app.use(express.json({limit:"10mb"}));
 app.use(cookieParser());
 
