efdc20846a5d740147a37b94ed6ed51e30e8d9fa
sanjaykumar9318 <sanjayswiz26@gmail.com>
2026-07-16 00:29:43 +0530

    deployement req changes

--- backend/src/index.js
+++ backend/src/index.js
@@ -9,7 +9,7 @@ import { app,server } from "./lib/socket.js"
 import path from "path"
 
 dotenv.config();
-
+const __dirname = path.resolve;
 app.use(express.json({limit:"10mb"}));
 app.use(cookieParser());
 
@@ -23,6 +23,17 @@ app.use(cors({
 app.use("/api/auth",authroutes)
 app.use("/api/messages",messageroutes)
 
+if (process.env.NODE_ENV === "production") {
+  app.use(
+    express.static(path.join(__dirname, "../../frontend/dist"))
+  );
+
+  app.get("*", (req, res) => {
+    res.sendFile(
+      path.join(__dirname, "../../frontend/dist/index.html")
+    );
+  });
+}
 
 server.listen(5001,()=>{
     console.log("server is connected sucessfully on 5001")
